80c82f9f61f70614b887f98c49472bcddd7aa76c
ramadani69 <mrhai1818@gmail.com>
2026-08-19 14:44:50 +0700

    Update halaman kontak dan perbaikan desain CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const header = document.querySelector(".header");
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", ()=>{
+/* ================= STICKY NAVBAR ================= */
 
-    header.classList.toggle("sticky", window.scrollY > 80);
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 80);
+});
 
+/* ================= MOBILE MENU ================= */
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+/* ================= CLOSE MENU AFTER CLICK ================= */
+
+const navLinks = document.querySelectorAll(".navbar a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
 });
